9b097bf574e641950feebb569a3f35dd789578eb
howlinweed <me@you>
2018-04-21 19:41:17 -0400

    installer update
    Should make the installer a bit more flexible.

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@
 SET MODNAME=AGOT-MMC
 SET OLDDIR=%CD%
 COPY /Y "%MODNAME%.mod" ..\ > nul
-REM CD ..\
+RMDIR "..\%MODNAME%" > nul
 MKLINK /J "..\%MODNAME%" "%OLDDIR%" > nul
 ECHO The mod has probably been installed. Give it a shot.
 PAUSE
